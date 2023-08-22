051db4e9f56a08f5351a05e0320db82bc4d01d56
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-23 02:42:44 +0300

    Clean up code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-
 import React, { useEffect, useState } from "react";
-import { Routes ,Route, useNavigate} from 'react-router-dom';
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Login from "./components/Login";
 import NavBar from "./components/NavBar";
 import Form from "./components/Form";
 import Firststep from "./components/Firststep";
-import './App.css';
+import "./index.css";
 import Register from "./components/Register";
 import { useLocalStorage } from "./useLocalStorage";
 import Answers from "./components/Answers";
 
-
-
 function App() {
   // const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [token, setToken] = useLocalStorage()
-  const [user, setUser] = useState(null)
+  const [token, setToken] = useLocalStorage();
+  const [user, setUser] = useState(null);
   const [fomData, setFomData] = useState({
     you: "",
     do: "",
@@ -26,50 +23,52 @@ function App() {
     beneficiaries: "",
     transform: "",
     income: "",
-    
   });
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const getMe = ()=>{
-    fetch("http://localhost:4000/users/me",{
-      headers:{
-        Authorization: `Bearer ${token}`
+  const getMe = () => {
+    fetch("http://localhost:4000/users/me", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((user) => {
+          setUser(user);
+          navigate("/");
+        });
+      } else {
+        console.log(r);
+        navigate("/login");
       }
-    }).then(r=>{
-      if(r.ok){
-        r.json().then(user=>{
-          setUser(user)
-          navigate("/")
-        })
-      }else{
-        console.log(r)
-        navigate("/login")
-      }
-    })
-  }
+    });
+  };
 
-  useEffect(()=>{
-    console.log(token)
-    getMe()
-  },[token])
+  useEffect(() => {
+    console.log(token);
+    getMe();
+  }, [token]);
   return (
-    <div className="App">
-    {<NavBar setToken={setToken} /> }
+    <div className='App'>
+      {<NavBar setToken={setToken} />}
 
       <Routes>
-      
-      <Route exact path="/navbar" element={<NavBar  setToken={setToken}/>} />
-      <Route exact path="/about" element={<About />} />
-      <Route exact path="/login"   element={<Login setToken={setToken} />} />
-      <Route exact path="/form" element={<Form  token={token}/>} />
-      <Route path="/firststep" element={<Firststep />} />
-      <Route exact path="/register" element={<Register setToken={setToken}/>} />
-      <Route exact path="/answers" element={<Answers fomData={fomData}  />} />
-      <Route exact path="/" element={<Home />} />
-        </Routes>
+        <Route exact path='/navbar' element={<NavBar setToken={setToken} />} />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/login' element={<Login setToken={setToken} />} />
+        <Route exact path='/form' element={<Form token={token} />} />
+        <Route path='/firststep' element={<Firststep />} />
+        <Route
+          exact
+          path='/register'
+          element={<Register setToken={setToken} />}
+        />
+        <Route exact path='/answers' element={<Answers fomData={fomData} />} />
+        <Route exact path='/' element={<Home />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
