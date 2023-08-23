4df54494540a45cf3461d01375c125c709f97bce
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-23 06:23:45 +0300

    Handle route protection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate, Outlet, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Login from "./components/Login";
@@ -10,11 +10,12 @@ import "./index.css";
 import Register from "./components/Register";
 import { useLocalStorage } from "./useLocalStorage";
 import Answers from "./components/Answers";
+import { useSelector } from "react-redux";
 
 function App() {
   // const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [token, setToken] = useLocalStorage();
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
   const [fomData, setFomData] = useState({
     you: "",
     do: "",
@@ -25,50 +26,40 @@ function App() {
     income: "",
   });
 
+  const user = useSelector((state) => state.user);
+  const { userInfo } = user;
+
   const navigate = useNavigate();
 
-  const getMe = () => {
-    fetch("http://localhost:4000/users/me", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }).then((r) => {
-      if (r.ok) {
-        r.json().then((user) => {
-          setUser(user);
-          navigate("/");
-        });
-      } else {
-        console.log(r);
-        navigate("/login");
-      }
-    });
-  };
+  function AuthLayout() {
+    if (userInfo.token) {
+      return <Outlet />;
+    }
+    return <Navigate to='/login' />;
+  }
 
-  useEffect(() => {
-    console.log(token);
-    getMe();
-  }, [token]);
   return (
     <div className='App'>
       {<NavBar setToken={setToken} />}
       <div className='main-cont'>
         <Routes>
-          <Route exact path='/about' element={<About />} />
-          <Route exact path='/login' element={<Login setToken={setToken} />} />
-          <Route exact path='/form' element={<Form token={token} />} />
-          <Route path='/firststep' element={<Firststep />} />
           <Route
             exact
             path='/register'
             element={<Register setToken={setToken} />}
           />
-          <Route
-            exact
-            path='/answers'
-            element={<Answers fomData={fomData} />}
-          />
-          <Route exact path='/' element={<Home />} />
+          <Route exact path='/login' element={<Login setToken={setToken} />} />
+          <Route element={<AuthLayout />}>
+            <Route exact path='/about' element={<About />} />
+            <Route exact path='/form' element={<Form token={token} />} />
+            <Route path='/firststep' element={<Firststep />} />
+            <Route
+              exact
+              path='/answers'
+              element={<Answers fomData={fomData} />}
+            />
+            <Route exact path='/' element={<Home />} />
+          </Route>
         </Routes>
       </div>
     </div>
