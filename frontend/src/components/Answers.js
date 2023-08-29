23218968c785be1c66f146b28f8ac902b8ea23ad
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-29 19:25:59 +0300

    Format purpose statement component content

--- frontend/src/components/Answers.js
+++ frontend/src/components/Answers.js
@@ -1,48 +1,55 @@
 import React from "react";
 import Purposestatement from "./Purposestatement";
-import { useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function Answers() {
   const form = useSelector((state) => state.form);
   const { you, what, love, serve, beneficiaries, transform, income } = form;
-  const { state } = useLocation();
-
-  console.log(form);
 
   return (
-    <div style={{ backgroundColor: "aliceblue" }}>
-      <h2>Here are your answers</h2>
-      <br />
-      <h2>Who are you?</h2>
-      <p>{you}</p>
-      <h2>What do you do well?</h2>
-      <p>{what}</p>
-      <h2>What do you love to do?</h2>
-      <p>{love}</p>
-      <h2>Whom do you intend to serve?</h2>
-      <p>{serve}</p>
-      <h2>What do your beneficiaries need?</h2>
-      <p>{beneficiaries}</p>
-      <h2>How do your offerings transform your beneficiaries?</h2>
-      <p>{transform}</p>
-      <h2>What activities can generate income for you?</h2>
-      <p>{income}</p>
-
-      <div
-        class='card text-bg-primary mb-3'
-        style={{
-          // maxWidth: "60rem",
-          // marginLeft: "1000px",
-          marginRight: "0px",
-          // marginTop: "-420px",
-          color: "white",
-          backgroundColor: "maroon",
-          height: "750px",
-        }}
-      >
-        <div class='card-header'>Write your purpose-statement</div>
-        <div class='card-body'>{<Purposestatement />}</div>
+    <div className='container'>
+      <div className='answers-row'>
+        <div className='answers-left'>
+          <Purposestatement />
+        </div>
+        <div class='card answers-right text-bg-primary mb-3'>
+          <div class='card-header answers-header'>
+            Here are your answers to the 7 key questions, you can click at any
+            of them to edit.
+          </div>
+          <div class='card-body'>
+            <ul className='answers-ul'>
+              <li>
+                <h6>1. Who are you?</h6>
+                <p>a. {you}</p>
+              </li>
+              <li>
+                <h6>What do you do well?</h6>
+                <p>{what}</p>
+              </li>
+              <li>
+                <h6>What do you love to do?</h6>
+                <p>{love}</p>
+              </li>
+              <li>
+                <h6>Whom do you intend to serve?</h6>
+                <p>{serve}</p>
+              </li>
+              <li>
+                <h6>What do your beneficiaries need?</h6>
+                <p>{beneficiaries}</p>
+              </li>
+              <li>
+                <h6>How do your offerings transform your beneficiaries?</h6>
+                <p>{transform}</p>
+              </li>
+              <li>
+                <h6>What activities can generate income for you?</h6>
+                <p>{income}</p>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/Purposestatement.js
+++ frontend/src/components/Purposestatement.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { createItem } from "../redux/actions/purposeActions";
 
@@ -9,7 +9,6 @@ function Purposestatement() {
   const { loading, error, success } = purpose;
   const form = useSelector((state) => state.form);
   const { you, what, love, serve, beneficiaries, transform, income } = form;
-  const { state } = useLocation();
   const navigate = useNavigate();
 
   const [purposeData, setPurposeData] = useState({
@@ -39,47 +38,87 @@ function Purposestatement() {
 
   return (
     <div className='container'>
-      <div className='row final-cont'>
-        <p>
+      <div className='row final-cont purpose-desc-cont'>
+        <h6 className='h6 form-title'>
+          <span>
+            <img src='/purpose-icon.png' alt='...' />
+          </span>
+          The Purpose Statement
+        </h6>
+        <p className='purpose-guide-p'>
           Turn your responses to the 7 questions above into a purpose statement
-          using the struc- ture below.
-          <br /> The purpose statement must be a concise, memorable and
-          inspiring decla- ration.I will use my purpose,
-          <br /> "I inspire individuals and organisations to live meaningful
-          lives", to provide a structure for the purpose
-          <br /> statement.Purpose has three main dimen- sions: directional,
-          transformational and motivational. The
-          <br />
-          directional dimensionincludes two aspects (1) the inspired actions you
-          take or the path you follow, and <br />
-          (2) for whom you do it. For example, based on my purpose, the
-          directional dimension is "I inspire indi-
-          <br />
-          viduals and organisations". The transformational dimension includes
-          the benefits your beneficiaries
-          <br /> accrue from your actions. This dimension captures the intended
-          impact on the recipients. It focuses on
-          <br /> the difference one makes in people's lives and society: the
-          impact. For example, the transformational
-          <br /> dimension of my purpose is "to live meaning- ful lives". The
-          motivational dimension is derived from the
-          <br /> language and worthiness of the cause as captured through the
-          directional and transformational dime-
-          <br />
-          nsions. Both direc- tional and transformational dimensions should be
-          inspiring to awaken motivation.
-          <br />
-          The directional and transformational aspects must be noble, compelling
-          and inspirational. It should
-          <br /> enlist or awaken the energies and enthusiasm you need to make
-          your purpose a reality. Purpose comes
-          <br /> with its energy. It is self-energising.
+          using the structure below.
         </p>
-
+        <p>
+          The purpose statement must be a concise, memorable and inspiring
+          declaration. I will use my purpose;{" "}
+          <i>
+            I inspire individuals and organisations to live meaningful lives,{" "}
+          </i>
+          to provide a structure for the purpose
+          <br /> statement.
+        </p>
+        <p>
+          Purpose has <strong>three</strong> main dimensions;
+          <ul>
+            <li>
+              <strong>Directional;</strong>
+              <p>
+                The directional dimension includes two aspects;
+                <ol>
+                  <li>
+                    The inspired actions you take or the path you follow, and
+                  </li>
+                  <li>For whom you do it.</li>
+                </ol>
+                For example, based on my purpose, the directional dimension is
+                <i> I inspire individuals and organisations.</i>
+              </p>
+            </li>
+            <li>
+              <strong>Transformational;</strong>
+              <p>
+                The transformational dimension includes the benefits your
+                beneficiaries accrue from your actions.
+              </p>
+              <p>
+                This dimension captures the intended impact on the recipients.
+              </p>
+              <p>
+                It focuses on the difference one makes in people's lives and
+                society; the impact.
+              </p>
+              <p>
+                For example, the transformational dimension of my purpose is{" "}
+                <i>to live meaningful lives</i>.
+              </p>
+            </li>
+            <li>
+              <strong>Motivational;</strong>
+              <p>
+                The motivational dimension is derived from the language and
+                worthiness of the cause as captured through the directional and
+                transformational dimensions.
+              </p>
+            </li>
+          </ul>
+        </p>
+        <p>
+          Both <i>directional</i> and <i>transformational</i> dimensions should
+          be inspiring to awaken <i>motivation.</i>
+        </p>
+        <p>
+          The directional and transformational aspects must be noble, compelling
+          and inspirational. It should enlist or awaken the energies and
+          enthusiasm you need to make your purpose a reality.
+        </p>
+        <p className='mb-1'>
+          <i>Purpose comes with its energy. It is self-energising.</i>
+        </p>
         <div class='mb-3'>
           <textarea
             class='form-control'
-            placeholder='purposestatement'
+            placeholder='Write your purpose statement here...'
             rows='8'
             value={purposeData.purposestatement}
             onChange={(e) => {
@@ -98,7 +137,6 @@ function Purposestatement() {
             type='button'
             class='btn btn-secondary mt-4'
             onClick={handleSubmit}
-            style={{ backgroundColor: "grey", color: "white" }}
           >
             Submit
           </button>
