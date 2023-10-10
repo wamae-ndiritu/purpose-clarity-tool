26a0d4215dfdc12ce7618ba8abe59019bce9229f
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-10-10 15:17:23 +0300

    Implement sharing PCT responses

--- frontend/v2/src/components/ShareModal.jsx
+++ frontend/v2/src/components/ShareModal.jsx
@@ -1,39 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import emailjs from "@emailjs/browser";
 import { useSelector } from "react-redux";
+import Loading from "../utils/Loading";
 
 const serviceID = process.env.REACT_APP_SERVICE_ID;
 const templateID = process.env.REACT_APP_TEMPLATE_ID;
 const publicKey = process.env.REACT_APP_PUBLIC_KEY;
 
 const ShareModal = ({ closeModal }) => {
-  const purposeStory = useSelector((state) => state.purposeStory);
-  const { item } = purposeStory;
+  const form = useSelector((state) => state.form);
+  const {
+    identity,
+    strengths,
+    passions,
+    target_audience,
+    beneficiary_needs,
+    impact,
+    revenue_sources,
+    purpose_statement,
+  } = form;
 
   const questions = {
-    question_1: "Purpose Statement",
-    question_2: "Origin and Personal Connection",
-    question_3: "Vision and Mission",
-    question_4: "Values and Beliefs",
-    question_5: "Impact and Beneficiaries",
-    question_6: "Actions and Commitments",
+    question_1: "Who are you?",
+    question_2: "What do you do well?",
+    question_3: "What do you love to do?",
+    question_4: "Whom do you intend to serve?",
+    question_5: "What do your beneficiaries need?",
+    question_6: "How do your offerings transform your beneficiaries?",
+    question_7: "What activities can generate income for you?",
+    question_8: "The Purpose Statement",
   };
 
   const answers = {
-    answer_1: item?.purpose,
-    answer_2: item?.origin,
-    answer_3:
-      item?.vissionAndMission?.vision +
-      item?.vissionAndMission?.mission +
-      item?.vissionAndMission?.impact,
-    answer_4: item?.valuesAndBeliefs?.values + item?.valuesAndBeliefs?.beliefs,
-    answer_5:
-      item?.impactAndBeneficiaries?.impact +
-      item.impactAndBeneficiaries?.beneficiaries,
-    answer_6:
-      item?.actionsAndCommitments?.actions +
-      item?.actionsAndCommitments?.examples +
-      item?.actionsAndCommitments?.dedication,
+    answer_1: identity,
+    answer_2: strengths,
+    answer_3: passions,
+    answer_4: target_audience,
+    answer_5: beneficiary_needs,
+    answer_6: impact,
+    answer_7: revenue_sources,
+    answer_8: purpose_statement,
   };
 
   const templateParams = {
@@ -43,8 +49,10 @@ const ShareModal = ({ closeModal }) => {
     ...questions,
   };
 
-  console.log(templateParams);
+  const [loading, setLoading] = useState(false);
+
   const shareResponses = () => {
+    setLoading(true);
     emailjs
       .send(
         "service_iz21rs6",
@@ -53,6 +61,7 @@ const ShareModal = ({ closeModal }) => {
         "KehPQQgwKRR6ja30g"
       )
       .then((response) => {
+        setLoading(false);
         console.log(response.text);
         closeModal();
       })
@@ -85,7 +94,8 @@ const ShareModal = ({ closeModal }) => {
           </svg>
         </button>
         <div>
-          <h2 class='text-2xl font-semibold mb-4'>Share your Story</h2>
+          <h2 class='text-2xl font-semibold mb-4'>Share your Responses</h2>
+          {loading && <Loading />}
           <p class='text-gray-700 mb-6'>
             By consenting to share your responses with the coach, you are
             agreeing to receive feedback aimed at enhancing your answers for a
